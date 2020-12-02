a4492e2b05378f9f1526d16eeab12996adeed0fa
Johannes Löthberg <johannes@kyriasis.com>
2020-12-03 00:48:49 +0100

    2020: Add day 2 solution
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

---
+++ 2020/src/bin/2020-day-2.rs
@@ -0,0 +1,93 @@
+use std::io::BufRead;
+
+use nom::IResult;
+
+#[derive(Debug)]
+struct PasswordSpecification<'a> {
+    min: usize,
+    max: usize,
+    letter: char,
+    password: &'a str,
+}
+
+fn parse_line(input: &str) -> IResult<&str, PasswordSpecification> {
+    let (input, min) = nom::combinator::map_res(nom::character::complete::digit1, |out: &str| {
+        usize::from_str_radix(&out, 10)
+    })(input)?;
+
+    let (input, _) = nom::bytes::complete::tag("-")(input)?;
+
+    let (input, max) = nom::combinator::map_res(nom::character::complete::digit1, |out: &str| {
+        usize::from_str_radix(&out, 10)
+    })(input)?;
+
+    let (input, _) = nom::bytes::complete::tag(" ")(input)?;
+
+    let (input, letter) = nom::character::complete::anychar(input)?;
+
+    let (input, _) = nom::bytes::complete::tag(": ")(input)?;
+
+    let (input, password) = nom::character::complete::not_line_ending(input)?;
+
+    Ok((
+        input,
+        PasswordSpecification {
+            min,
+            max,
+            letter,
+            password,
+        },
+    ))
+}
+
+fn password_is_valid_down_the_street(spec: &PasswordSpecification) -> bool {
+    let matches = spec.password.chars().filter(|&c| c == spec.letter).count();
+    if matches < spec.min || matches > spec.max {
+        false
+    } else {
+        true
+    }
+}
+
+fn password_is_valid_north_pole_toboggan(spec: &PasswordSpecification) -> bool {
+    let min_matches = match spec.password.chars().nth(spec.min - 1) {
+        Some(c) => c == spec.letter,
+        None => false,
+    };
+
+    let max_matches = match spec.password.chars().nth(spec.max - 1) {
+        Some(c) => c == spec.letter,
+        None => false,
+    };
+
+    min_matches ^ max_matches
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().filter_map(|l| l.ok()).collect();
+
+    let specs: Vec<PasswordSpecification> = lines
+        .iter()
+        .map(|l| parse_line(l.as_ref()))
+        .filter_map(|l| l.ok())
+        .map(|(_, spec)| spec)
+        .collect();
+
+    let valid_down_the_street = specs
+        .iter()
+        .map(password_is_valid_down_the_street)
+        .filter(|&is_valid| is_valid)
+        .count();
+    println!("Valid passwords down the street: {}", valid_down_the_street);
+
+    let valid_north_pole_toboggan = specs
+        .iter()
+        .map(password_is_valid_north_pole_toboggan)
+        .filter(|&is_valid| is_valid)
+        .count();
+    println!(
+        "Valid passwords at North Pole Toboggan: {}",
+        valid_north_pole_toboggan
+    );
+}
