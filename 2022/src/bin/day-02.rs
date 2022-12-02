54281addee6dfface216956873f541fcb715f9aa
Johannes Löthberg <johannes@kyriasis.com>
2022-12-02 22:10:16 +0100

    2022: Add part 2 of day 02
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- 2022/src/bin/day-02.rs
+++ 2022/src/bin/day-02.rs
@@ -5,6 +5,17 @@ enum Outcome {
     Lose,
 }
 
+impl From<&str> for Outcome {
+    fn from(value: &str) -> Self {
+        match value {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => panic!("Oh no"),
+        }
+    }
+}
+
 impl Outcome {
     fn score(self) -> usize {
         match self {
@@ -33,6 +44,18 @@ impl Move {
         }
     }
 
+    fn calculate_move(wanted_outcome: Outcome, opponents_move: Move) -> Self {
+        match (wanted_outcome, opponents_move) {
+            (Outcome::Win, Move::Rock) => Move::Paper,
+            (Outcome::Win, Move::Paper) => Move::Scissors,
+            (Outcome::Win, Move::Scissors) => Move::Rock,
+            (Outcome::Draw, _) => opponents_move,
+            (Outcome::Lose, Move::Rock) => Move::Scissors,
+            (Outcome::Lose, Move::Paper) => Move::Rock,
+            (Outcome::Lose, Move::Scissors) => Move::Paper,
+        }
+    }
+
     fn score(self) -> usize {
         match self {
             Move::Rock => 1,
@@ -42,38 +65,39 @@ impl Move {
     }
 }
 
-impl TryFrom<&str> for Move {
-    type Error = ();
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let m = match value {
+impl From<&str> for Move {
+    fn from(value: &str) -> Self {
+        match value {
             "A" => Move::Rock,
             "B" => Move::Paper,
             "C" => Move::Scissors,
             "X" => Move::Rock,
             "Y" => Move::Paper,
             "Z" => Move::Scissors,
-            _ => return Err(()),
-        };
-        Ok(m)
+            _ => panic!("Oh no"),
+        }
     }
 }
 
 fn main() {
-    let score: usize = std::io::stdin()
+    let (part1_score, part2_score) = std::io::stdin()
         .lines()
         .map(|line| {
             let line = line.unwrap();
-            let mut mapped = line.split_whitespace().map(Move::try_from);
-            (
-                mapped.next().unwrap().unwrap(),
-                mapped.next().unwrap().unwrap(),
-            )
+            let mut iter = line.split_whitespace();
+            let first = iter.next().unwrap();
+            let second = iter.next().unwrap();
+
+            (Move::from(first), Move::from(second), Outcome::from(second))
         })
-        .map(|(opponent, mine)| {
-            let outcome = mine.beats(opponent);
-            mine.score() + outcome.score()
+        .map(|(opponents_move, my_move, wanted_outcome)| {
+            let part1 = my_move.score() + my_move.beats(opponents_move).score();
+
+            let part2_my_move = Move::calculate_move(wanted_outcome, opponents_move);
+            let part2 = part2_my_move.score() + part2_my_move.beats(opponents_move).score();
+
+            (part1, part2)
         })
-        .sum();
-    println!("Score: {}", score);
+        .fold((0, 0), |(p1, p2), (n1, n2)| (p1 + n1, p2 + n2));
+    println!("Scores: {} {}", part1_score, part2_score);
 }
