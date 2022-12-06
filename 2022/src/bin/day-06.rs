5f357e0a9381696929dc5b75236a0f1c40802ae8
Johannes Löthberg <johannes@kyriasis.com>
2022-12-06 21:33:59 +0100

    2022: Add part 2 of day 06
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- 2022/src/bin/day-06.rs
+++ 2022/src/bin/day-06.rs
@@ -3,10 +3,17 @@ use odds::string::StrChunksWindows;
 
 fn main() {
     let line = std::io::stdin().lines().next().unwrap().unwrap();
-    let (idx, _) = line
+    let (start_of_packet_idx, _) = line
         .char_windows(4)
         .enumerate()
         .find(|(_, slice)| slice.chars().unique().count() == 4)
         .unwrap();
-    println!("First no-duplicate sequence at index {}", idx + 4);
+    println!("Start-of-packet at: {}", start_of_packet_idx + 4);
+
+    let (start_of_message_idx, _) = line
+        .char_windows(14)
+        .enumerate()
+        .find(|(_, slice)| slice.chars().unique().count() == 14)
+        .unwrap();
+    println!("Start-of-message at {}", start_of_message_idx + 14);
 }
